634b40d7d953437ff56d8e5904d4f291919569c6
maxchebib <maxime.chebib@ecl21.ec-lyon.fr>
2024-09-27 08:36:34 +0900

    Add Attention UNET

--- code/model.py
+++ code/model.py
@@ -16,12 +16,51 @@ class DoubleConv(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
+    
+class AttentionBlock(nn.Module):
+    """Attention block with learnable parameters"""
 
-class UNET(nn.Module):
+    def __init__(self, F_g, F_l, n_coefficients):
+        """
+        :param F_g: number of feature maps (channels) in previous layer
+        :param F_l: number of feature maps in corresponding encoder layer, transferred via skip connection
+        :param n_coefficients: number of learnable multi-dimensional attention coefficients
+        """
+        super(AttentionBlock, self).__init__()
+        self.W_gate = nn.Sequential(
+            nn.Conv2d(F_g, n_coefficients, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(n_coefficients)
+        )
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, n_coefficients, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(n_coefficients)
+        )
+        self.psi = nn.Sequential(
+            nn.Conv2d(n_coefficients, 1, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, gate, skip_connection):
+        """
+        :param gate: gating signal from previous layer
+        :param skip_connection: activation from corresponding encoder layer
+        :return: output activations
+        """
+        g1 = self.W_gate(gate)
+        x1 = self.W_x(skip_connection)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        out = skip_connection * psi
+        return out
+
+
+class VanillaUNET(nn.Module):
     def __init__(
             self, in_channels =1, out_channels=1, features=[64,128,256,512]
     ):
-        super(UNET, self).__init__()
+        super(VanillaUNET, self).__init__()
         self.ups = nn.ModuleList()
         self.downs = nn.ModuleList() #used to store the convolutional layers etc
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -66,9 +105,51 @@ class UNET(nn.Module):
         
         return self.final_conv(x)
     
+class AttentionUNET(nn.Module):
+    def __init__(
+            self, in_channels=3, out_channels=1, features=[64,128,256,512]
+    ):
+        super(AttentionUNET, self).__init__()
+        self.downs = nn.ModuleList() #used to store the convolutional layers etc
+        self.ups = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Down part of UNET
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        # Up part of UNET
+        for feature in reversed(features):
+            self.ups.append(nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2))
+            self.ups.append(AttentionBlock(F_g=feature, F_l=feature, n_coefficients=feature//2))
+            self.ups.append(DoubleConv(feature*2, feature))
+
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self,x):
+        skip_connections = []
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        # Decoder (upsampling) path
+        for i in range(0, len(self.ups), 3):
+            x = self.ups[i](x)  # ConvTranspose2d
+            skip = self.ups[i+1](gate=x, skip_connection=skip_connections[i//3])  # AttentionBlock
+            x = torch.cat((skip, x), dim=1)
+            x = self.ups[i+2](x)  # DoubleConv
+
+        return self.final_conv(x)
+    
 def test():
-    x = torch.randn((3,1,160,160))
-    model = UNET(in_channels=1, out_channels=1)
+    x = torch.randn((3,1,128,128))
+    model = AttentionUNET(in_channels=1, out_channels=1)
     preds = model(x)
     print(preds.shape)
     print(x.shape)
